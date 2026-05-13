3cb76d78fdba9ee141e6e9a15f7809be0ec951d7
Hanamaraddi <hanamaraddi@email.com>
2026-05-13 10:07:18 +0530

    Completed initial ML setup and data loading

---
+++ house_price_prediction.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("house_prices_bangalore.csv")
+
+# Show first 5 rows
+print(df.head())
